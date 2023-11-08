8210e74e947a4ce1de900bf0e08f3a10d0bca0c5
John Carlo Namor <carlokun1999@gmail.com>
2023-11-08 20:58:07 +0800

    update

---
+++ info.js
@@ -0,0 +1,94 @@
+export const users = [
+    {
+      name: 'JOHN CARLO B. NAMOR',
+      nickName:'Loy',
+      address:'South Korea',
+      courseAndYear:'BSIT-3',
+      avatar:
+        'https://1739752386.rsc.cdn77.org/data/images/full/246173/kim-soo-hyun.png?w=600?w=430',
+    },
+    {
+        name: 'LOLAAFLORAA',
+        nickName:'Aming',
+        address:'Somewhere',
+        courseAndYear:'HM-4',
+        avatar:
+          'https://scontent-mnl1-1.xx.fbcdn.net/v/t1.15752-9/375021034_6796842080409118_6228409176377630753_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeFFN416-wCbiKCeGEzbTA1BoDi5ESymnPqgOLkRLKac-qTaUX9BBHXgyoyCMvgPyqhundRyJvuulFiaGgD_e66R&_nc_ohc=rAeqrqp5mI8AX95Z2ro&_nc_ht=scontent-mnl1-1.xx&oh=03_AdTcRhIODX_un_kOfGRZ79HHKZQnjPPzdh9v_WDh_48Biw&oe=6572F6BF',
+      },
+      {
+        name: 'PROFILE',
+        nickName:'anon',
+        address:'Location',
+        courseAndYear:'anon',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        name: 'PROFILE',
+        nickName:'anon',
+        address:'Location',
+        courseAndYear:'anon',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        name: 'PROFILE',
+        nickName:'anon',
+        address:'Location',
+        courseAndYear:'anon',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        name: 'PROFILE',
+        nickName:'anon',
+        address:'Location',
+        courseAndYear:'anon',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        name: 'PROFILE',
+        nickName:'anon',
+        address:'Location',
+        courseAndYear:'anon',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        name: 'PROFILE',
+        nickName:'anon',
+        address:'Location',
+        courseAndYear:'anon',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        name: 'PROFILE',
+        nickName:'anon',
+        address:'Location',
+        courseAndYear:'anon',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        name: 'PROFILE',
+        nickName:'anon',
+        address:'Location',
+        courseAndYear:'anon',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        name: 'PROFILE',
+        nickName:'anon',
+        address:'Location',
+        courseAndYear:'anon',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      
+     
+
+    
+  ];
